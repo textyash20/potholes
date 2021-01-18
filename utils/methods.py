c59951c74c203e8d398758259907ed0897b5914b
yash kadam <textyash20@gmail.com>
2021-01-19 00:09:21 +0530

    Create methods.py

---
+++ utils/methods.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+import os
+from flask_cors import CORS, cross_origin
+#import flask_monitoringdashboard as dashboard
+import json
+
